af2d923fd665ef971810c5c6fe55f6adb4a45f91
erye <erye@tiiit.cn>
2019-12-15 15:18:05 +0800

    :sparkles: Add mainmenu
    1. Render mainmenu through route configuration

--- packages/demo/src/router/index.js
+++ packages/demo/src/router/index.js
@@ -1,20 +1,47 @@
 const routes = [
-  { path: '/', redirect: '/list/index' },
-  { path: '/home', component: () => import('../pages/index.vue') },
-  { path: '/login', component: () => import('../pages/login.vue') },
   {
-    path: '/list',
+    path: '/',
+    name: 'root',
     redirect: '/list/index',
+    hidden: true,
+  },
+  {
+    path: '/home',
+    name: 'home',
+    hidden: false,
+    subMenu: false,
+    component: () => import('../pages/index.vue'),
+    meta: {
+      name: '首页',
+      title: '首页',
+      keepAlive: false,
+      fontIcon: 'el-icon-s-home',
+    },
+  },
+  {
     // component 这里会填充上FirstFloorLayout
+    path: '/list',
+    name: 'list',
+    hidden: false,
+    subMenu: true,
+    redirect: '/list/index',
+    meta: {
+      name: '列表',
+      title: '列表',
+      keepAlive: false,
+      fontIcon: 'el-icon-menu',
+    },
     children: [
       {
         path: 'index',
+        hidden: false,
         component: () => import('../pages/list/index.vue'),
       },
       {
-        path: 'detail',
-        redirect: '/list/detail/1',
         // component 这里会填充上SecondFloorLayout
+        path: 'detail',
+        hidden: false,
+        redirect: '/list/detail/1',
         children: [
           {
             path: ':id',
--- packages/vue-admin-framework/src/layout/cellFirstFloorLayout/cellSidebar/MainMenu.vue
+++ packages/vue-admin-framework/src/layout/cellFirstFloorLayout/cellSidebar/MainMenu.vue
@@ -1,34 +1,149 @@
 <template>
-  <div class="main-menu">
-    <el-scrollbar wrap-class="scrollbar-wrapper">
-      <div class="main-menu-item"></div>
-    </el-scrollbar>
-  </div>
+  <el-scrollbar class="main-menu-scrollbar" wrap-class="scrollbar-wrapper">
+    <ul class="main-menu">
+      <li
+        class="main-menu-item"
+        :class="{ active: selectedName === item.name }"
+        v-for="item in list"
+        :key="item.path"
+        :title="item.label"
+        @click.stop="handleClick(item)"
+        @mouseenter.stop="handleMouseEnter(item)"
+        @mouseleave.stop="handleMouseLeave(item)"
+      >
+        <i class="main-menu-icon" :class="item.icon"></i>
+        <span class="main-menu-label">{{ item.label }}</span>
+      </li>
+    </ul>
+  </el-scrollbar>
 </template>
 
 <script>
 import globalData from '@/helper/globalData';
 
-let route = [];
+let _routes = [];
 
 export default {
   name: 'MainMenu',
   data() {
-    return {};
+    return {
+      list: [],
+      selectedName: '',
+    };
+  },
+  watch: {
+    $route(next) {
+      this.selectedName = next.matched[0].name;
+    },
   },
   created() {
-    route = globalData.get('routeConfig');
+    const routeConfig = globalData.get('routeConfig', {});
+    _routes = routeConfig.routes || [];
+
+    this.selectedName = this.$route.matched[0].name;
+    this.list = _routes
+      .filter(({ hidden }) => !hidden)
+      .map(({ name, path, hidden, meta }) => {
+        return {
+          name,
+          path,
+          label: meta.name || meta.title,
+          icon: meta.fontIcon,
+        };
+      });
+  },
+  methods: {
+    handleClick(item) {
+      this.$emit('click');
+      this.selectedName = item.name;
+      this.$router.push(item.path);
+    },
+    handleMouseEnter(item) {
+      const { name } = item;
+      if (this.selectedName !== name) {
+        this.$emit('enter', name);
+      }
+    },
+    handleMouseLeave(item) {
+      const { name } = item;
+      if (this.selectedName !== name) {
+        this.$emit('leave', name);
+      }
+    },
   },
 };
 </script>
 
 <style lang="less" scoped>
 @import '~@/style/variables';
-.main-menu /deep/ {
-  margin: 0;
-  padding: 8px 0;
+
+.main-menu-scrollbar /deep/ {
+  height: calc(100vh - @navbarHeight);
+
+  // Vue style 深度作用选择器 >>> 与 /deep/（sass/less）
+  // style       >>>
+  // less|sass   /deep/
+  /deep/ .scrollbar-wrapper {
+    overflow-x: hidden !important;
+  }
+  .main-menu {
+    margin: 0;
+    padding: 8px 0;
+  }
 }
 
 .main-menu-item /deep/ {
+  cursor: pointer;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 0 16px;
+  height: 40px;
+  line-height: 40px;
+  font-weight: 700;
+  color: rgb(102, 102, 102);
+
+  .main-menu-icon {
+    flex-shrink: 0;
+    font-size: 16px;
+    color: #999;
+  }
+
+  .main-menu-label {
+    margin-left: 10px;
+    font-size: 14px;
+    overflow: hidden;
+    word-wrap: normal;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+}
+
+.main-menu-item:hover {
+  background: #f3f3f3;
+}
+
+.main-menu-item {
+  &.active,
+  &:hover {
+    .main-menu-icon {
+      color: #fd5149;
+    }
+  }
+
+  &.active {
+    color: #fd5149;
+  }
+}
+
+.hideSidebar {
+  .main-menu-item {
+    padding: 0;
+    justify-content: center;
+
+    .main-menu-label {
+      display: none;
+    }
+  }
 }
 </style>
